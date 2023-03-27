aa78500df024e4173a976507a0ae72407fc0af2f
ulaş eren <ulass._eren97@gmail.com>
2023-03-27 10:17:08 +0300

    Added userprofile service security

---
+++ user-service/src/main/java/com/ulas/config/security/JwtTokenFilter.java
@@ -0,0 +1,42 @@
+package com.ulas.config.security;
+
+import com.ulas.exception.ErrorType;
+import com.ulas.exception.UserServiceException;
+import com.ulas.utility.JwtTokenManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtTokenManager jwtTokenManager;
+    @Autowired
+    private JwtUserDetails jwtUserDetails;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        //System.out.println("====>"+authorizationHeader);
+        if(authorizationHeader!=null&&authorizationHeader.startsWith("Bearer ")&& SecurityContextHolder.getContext().getAuthentication()==null){
+            String token = authorizationHeader.substring(7);
+            Optional<String> userRole = jwtTokenManager.getRoleFromToken(token);
+            if(userRole.isPresent()){//jwtTokenManager.validateToken(token)
+                UserDetails userDetails = jwtUserDetails.loadUserByUserRole(userRole.get());
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }else{
+                throw new UserServiceException(ErrorType.INVALID_TOKEN);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}
